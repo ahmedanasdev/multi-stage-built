3fe6f98ca96879ad91ea25c9cd98ebea5b7c2ac9
Ahmed Anas <aanas@localhost.localdomain>
2025-08-30 20:55:42 +0300

    Revert "break build to test ci"
    This reverts commit 9a35e02f77f242a74c240380313ca03fbb1f3366.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-//import logo from './logo.svg';
-//import './App.css';
-
-import React from 'react'
-import testcifail from 'react'   
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
